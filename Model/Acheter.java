1de263359a6689db93cc5ac5f82dd028a397b073
Nathberrebi <berrebinathane@gmail.com>
2018-04-20 14:52:29 +0200

    new

--- Model/Acheter.java
+++ Model/Acheter.java
@@ -1,6 +1,5 @@
 public interface Acheter{
     public void Achete();
-    public void ajouterArticleAcheteP();
-    public void ajouterArticleAcheteM();
+    public void ajouterArticleAchete();
 
 }
