28a41851040907320a0df2200e35845c16dc91be
Apostolus <apostolus.pace@gmail.com>
2018-05-02 10:58:17 +0200

    Commande, Article modifiées avec test d'ajout d'une commande.

--- Model/Entite_Part/Entrepot.java
+++ Model/Entite_Part/Entrepot.java
@@ -5,9 +5,19 @@ public class Entrepot extends Professionnel{
     	 super(country,departement,city,specificAdress,email,phoneNumber,decouvert,montantDecouvertAutorise,montantDecouvert,solde,nom);
     
     }
+    
+    public Entrepot() {
+    	this(null,null,null,null,null,null,true,0,0,0,null);
+    }
 
 	@Override
 	public boolean Achete(Entrepot entrepot, Article article, int quantite) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean payer(Commande commande) {
 		// TODO Auto-generated method stub
 		return false;
 	}
