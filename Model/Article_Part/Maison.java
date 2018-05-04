71270a4994076500122b55e89438fd0c560bcb44
Nathberrebi <berrebinathane@gmail.com>
2018-05-04 16:09:15 +0200

    Voiture, electromenager, meuble , maison Faut gerer la garantie Et pour l'immobilier je me suis dis de verifier si lappartement etait meublé ou pas

---
+++ Model/Article_Part/Maison.java
@@ -0,0 +1,12 @@
+public class Maison extends Immobilier{
+    private int nbPiece;
+    private Meuble[] meubles;
+
+    public Maison(String type, String description, Time time, double price, double surface, int nbPiece, Meuble[] meubles) {
+        super("Maison", description, time, price, surface);
+        this.nbPiece = nbPiece;
+        this.meubles = meubles;
+    }
+
+
+}
