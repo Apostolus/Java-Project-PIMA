423f3aedfa414d7ef29b18f5b9c9b570905aa63e
Nathberrebi <berrebinathane@gmail.com>
2018-05-08 22:00:09 +0200

    fond de commerce

---
+++ Model/Article_Part/FondDeCommerce.java
@@ -0,0 +1,33 @@
+public class FondDeCommerce extends Immobilier{
+    private String nomCommerce;
+    private double chiffreDAffaire ;
+    private double coteEmplacement ; // int entre 0 et 100 pour noter la qualité de l'emplacement.
+
+    public FondDeCommerce(String type, String description, Time time, double price, double surface, int nbPiece, int nbMeubles, String nomCommerce, double chiffreDAffaire) {
+        super("Maison", description, time, price, surface);
+        this.nomCommerce = nomCommerce;
+        this.chiffreDAffaire = chiffreDAffaire;
+        price = (3*chiffreDAffaire)*(1+(coteEmplacement/100));
+    }
+
+    public String getNomCommerce() {
+        return nomCommerce;
+    }
+
+    public void setNomCommerce(String nomCommerce) {
+        this.nomCommerce = nomCommerce;
+    }
+
+    public double getChiffreDAffaire() {
+        return chiffreDAffaire;
+    }
+
+    public FondDeCommerce setChiffreDAffaire(double chiffreDAffaire) {
+        this.chiffreDAffaire = chiffreDAffaire;
+        return this;
+    }
+
+    public double getCoteEmplacement() {
+        return coteEmplacement;
+    }
+}
--- Model/Article_Part/Maison.java
+++ Model/Article_Part/Maison.java
@@ -2,11 +2,22 @@ public class Maison extends Immobilier{
     private int nbPiece;
     private Meuble[] meubles;
 
-    public Maison(String type, String description, Time time, double price, double surface, int nbPiece, Meuble[] meubles) {
+    public Maison(String type, String description, Time time, double price, double surface, int nbPiece, int nbMeubles) {
+        super("Maison", description, time, price, surface);
+        this.nbPiece = nbPiece;
+        meubles = new Meuble[nbMeubles];
+    }
+
+    public Maison(String type, String description, Time time, double price, double surface,int nbPiece, Meuble[] meubles) {
         super("Maison", description, time, price, surface);
         this.nbPiece = nbPiece;
         this.meubles = meubles;
     }
+
+    public int PriceLocationParMois(){
+        return price*(1.005);
+    }
+
 
 
 }
