5815d789fa19678ac966a8c766ad4b216092521e
[Apostolus] <apostolus.pace@gmail.com>
2018-05-10 16:35:36 +0200

    Regroupement de Tout

---
+++ Model/files_java/Vehicule.java
@@ -0,0 +1,13 @@
+public class Vehicule extends Mobilier{
+    private String[] options; // hque voiture a certaines options (climatisation, gps..)
+
+
+    public Vehicule(String type, String description, Time timeOfPublish, double price, int quantite, String nom, String marque, int tailleOptions) {
+        super("Vehicule",  description, timeOfPublish, price, quantite, nom, marque, null);
+        options = new String[tailleOptions];
+    }
+
+    public String[] getOptions() {
+        return options;
+    }
+}
